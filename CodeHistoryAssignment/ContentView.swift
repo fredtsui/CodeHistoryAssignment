793b7913726fd25f3bd202bdb0d02e81c2de4a82
Yellow Peril <phredonline@mac.com>
2024-02-27 14:29:12 -0800

    attempt1: first changes to commit.

--- CodeHistoryAssignment/ContentView.swift
+++ CodeHistoryAssignment/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentcolor = Color(red: 48/255, green: 105/255, blue:
+    240/255)
     
     var body: some View {
         Text("Take me to your leader!")
