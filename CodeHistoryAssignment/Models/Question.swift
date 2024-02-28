120b1570f4b53ee54149f9d8a59c4b4cd99edbfd
Yellow Peril <phredonline@mac.com>
2024-02-28 07:57:07 -0800

    finished Creating Game Model

--- CodeHistoryAssignment/Models/Question.swift
+++ CodeHistoryAssignment/Models/Question.swift
@@ -7,8 +7,31 @@
 
 import Foundation
 
-struct Question {
+struct Question: Hashable {
     let questionText: String
     let possibleAnswers: [String]
     let correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(
+            questionText: "Who invented the World Wide Web?",
+            possibleAnswers: ["Steve Jobs","Linus Torvalds","Bill Gates","Tim Berners-Lee"],
+            correctAnswerIndex: 3
+            ),
+
+        Question(
+            questionText: "What was the first object oriented programming language?",
+            possibleAnswers: ["Simula", "Python", "Swift", "C"],
+            correctAnswerIndex: 0
+            ),
+        
+        Question(
+            questionText: "What was the first computer bug?",
+            possibleAnswers: ["Ant","Beetle","Moth","Fly"],
+            correctAnswerIndex: 2
+            )
+
+        ]
 }
+
+    
